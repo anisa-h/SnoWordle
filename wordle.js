3525f760ab32371440beacf70198fe730a58775b
anisa-h <anisa.hossain13@gmail.com>
2025-07-08 15:25:50 -0400

    i finished the game

---
+++ wordle.js
@@ -0,0 +1,84 @@
+
+var height = 6; //number of guesses
+var width = 5; //length of word
+
+var row = 0; 
+var col = 0; 
+
+var gameOver = false; 
+var word = "Rhino"; //the word to guess
+
+
+window.onload = function() {
+    intialize(); 
+}
+
+function intialize() {
+
+    //create the board
+    // loop of 30, to create 6 rows and 5 columns
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile. innerText = "";
+            document.getElementById("Board").appendChild(tile);
+        }
+    }
+    //key presses
+    document.addEventListener("keyup", (e) => {
+        if (gameOver) return; //if game is over, do nothing
+
+        if ("KeyA" <= e.code && e.code <= "KeyZ") {
+            if (col < width) {
+                let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+                if (currentTile,innerText == "") {
+                    currentTile.innerText = e.code[3];
+                    col += 1;
+                                }
+                
+            }
+            else if (e.code == "Backspace") {
+                col -= 1;
+        }
+        let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+        currentTile.innerText = "";
+        }
+        
+        else if (e.code == "Enter") {
+            update();
+            row += 1;
+            col = 0; 
+        }
+
+        if (!gameOver && row == height) {
+            gameOver = true; 
+            document.getElementById("Answer").innerText = "The word was: " + word;
+        }
+    })
+    
+}   
+
+function update() {
+    let correct = 0;
+    for (let c =0; c< width; c++) {
+        let currentTile = document.getElementById(row.toString() + "-" + c.toString());
+        let letter = currentTile.innerText;
+
+        if (word[c].toLowerCase() == letter.toLowerCase()) { //if letter is correct
+            currentTile.classList.add("correct");
+            correct += 1;
+        }
+        else if (word.toLowerCase().includes(letter.toLowerCase())) { //if letter is in the word but not in the correct position
+            currentTile.classList.add("present");
+        }
+        else { //if letter is not in the word
+            currentTile.classList.add("absent");    
+        }
+        if (correct == width) { //if all letters are correct
+            gameOver = true; 
+            document.getElementById("Answer").innerText = "You win! The word was: " + word;
+        }
+    }
+}
